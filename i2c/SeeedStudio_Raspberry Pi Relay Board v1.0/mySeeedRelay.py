bc72ec1b63a248a05b353f87b599a5c183bfecce
celceta <oss@celceta.com>
2017-04-11 23:48:11 +0900

    for SeeedStudio

---
+++ i2c/SeeedStudio_Raspberry Pi Relay Board v1.0/mySeeedRelay.py
@@ -0,0 +1,134 @@
+#!/usr/bin/python
+
+# Original (Seeed Studio Wiki)
+# http://wiki.seeed.cc/Raspberry_Pi_Relay_Board_v1.0/
+
+# Modified by celceta
+#
+# My site
+# https://github.com/celceta/pi
+
+import signal
+import sys
+
+import smbus
+
+bus = smbus.SMBus(1)  # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
+
+class Relay():
+    global bus
+
+    def __init__(self):
+        self.DEVICE_ADDRESS = 0x20  # 7 bit address (will be left shifted to add the read write bit)
+        self.DEVICE_REG_MODE1 = 0x06
+        self.DEVICE_REG_DATA = 0xff
+        bus.write_byte_data(self.DEVICE_ADDRESS, self.DEVICE_REG_MODE1, self.DEVICE_REG_DATA)
+
+    def ON_1(self):
+        print('ON_1...')
+        self.DEVICE_REG_DATA &= ~(0x1 << 0)
+        bus.write_byte_data(self.DEVICE_ADDRESS, self.DEVICE_REG_MODE1, self.DEVICE_REG_DATA)
+
+    def ON_2(self):
+        print('ON_2...')
+        self.DEVICE_REG_DATA &= ~(0x1 << 1)
+        bus.write_byte_data(self.DEVICE_ADDRESS, self.DEVICE_REG_MODE1, self.DEVICE_REG_DATA)
+
+    def ON_3(self):
+        print('ON_3...')
+        self.DEVICE_REG_DATA &= ~(0x1 << 2)
+        bus.write_byte_data(self.DEVICE_ADDRESS, self.DEVICE_REG_MODE1, self.DEVICE_REG_DATA)
+
+    def ON_4(self):
+        print('ON_4...')
+        self.DEVICE_REG_DATA &= ~(0x1 << 3)
+        bus.write_byte_data(self.DEVICE_ADDRESS, self.DEVICE_REG_MODE1, self.DEVICE_REG_DATA)
+
+    def OFF_1(self):
+        print('OFF_1...')
+        self.DEVICE_REG_DATA |= (0x1 << 0)
+        bus.write_byte_data(self.DEVICE_ADDRESS, self.DEVICE_REG_MODE1, self.DEVICE_REG_DATA)
+
+    def OFF_2(self):
+        print('OFF_2...')
+        self.DEVICE_REG_DATA |= (0x1 << 1)
+        bus.write_byte_data(self.DEVICE_ADDRESS, self.DEVICE_REG_MODE1, self.DEVICE_REG_DATA)
+
+    def OFF_3(self):
+        print('OFF_3...')
+        self.DEVICE_REG_DATA |= (0x1 << 2)
+        bus.write_byte_data(self.DEVICE_ADDRESS, self.DEVICE_REG_MODE1, self.DEVICE_REG_DATA)
+
+    def OFF_4(self):
+        print('OFF_4...')
+        self.DEVICE_REG_DATA |= (0x1 << 3)
+        bus.write_byte_data(self.DEVICE_ADDRESS, self.DEVICE_REG_MODE1, self.DEVICE_REG_DATA)
+
+    def ALLON(self):
+        print('ALL ON...')
+        self.DEVICE_REG_DATA &= ~(0xf << 0)
+        bus.write_byte_data(self.DEVICE_ADDRESS, self.DEVICE_REG_MODE1, self.DEVICE_REG_DATA)
+
+    def ALLOFF(self):
+        print('ALL OFF...')
+        self.DEVICE_REG_DATA |= (0xf << 0)
+        bus.write_byte_data(self.DEVICE_ADDRESS, self.DEVICE_REG_MODE1, self.DEVICE_REG_DATA)
+
+    def Control(self, dev, sw):
+        if '0' <= sw and sw <= '1':
+            if '1' <= dev and dev <= '4':
+                if sw == '0':
+                    self.DEVICE_REG_DATA |=  (0x01 << (int(dev)-1))
+                else:
+                    self.DEVICE_REG_DATA &= ~(0x01 << (int(dev)-1))
+                bus.write_byte_data(self.DEVICE_ADDRESS, self.DEVICE_REG_MODE1, self.DEVICE_REG_DATA)
+            else:
+                print('unknown relay target')
+        else:
+            print('unknown sw')
+
+if __name__ == "__main__":
+    relay = Relay()
+
+    # Called on process interruption. Set all pins to "Input" default mode.
+    def endProcess(signalnum=None, handler=None):
+        relay.ALLOFF()
+        sys.exit()
+
+
+    signal.signal(signal.SIGINT, endProcess)
+
+    if len(sys.argv) == 3:
+        if sys.argv[1] == 'all' and sys.argv[2] == '0':
+            relay.ALLOFF()
+        elif sys.argv[1] == 'all' and sys.argv[2] == '1':
+            relay.ALLON()
+        else:
+            relay.Control(sys.argv[1], sys.argv[2])
+        sys.exit()
+
+    while True:
+        ct = raw_input("input: ")
+        if ct == '1on':
+            relay.ON_1()
+        elif ct == '2on':
+            relay.ON_2()
+        elif ct == '3on':
+            relay.ON_3()
+        elif ct == '4on':
+            relay.ON_4()
+        elif ct == '1off':
+            relay.OFF_1()
+        elif ct == '2off':
+            relay.OFF_2()
+        elif ct == '3off':
+            relay.OFF_3()
+        elif ct == '4off':
+            relay.OFF_4()
+        elif ct == 'allon':
+            relay.ALLON()
+        elif ct == 'alloff':
+            relay.ALLOFF()
+        elif ct == 'quit' or ct == 'exit':
+            relay.ALLOFF()
+            sys.exit()
